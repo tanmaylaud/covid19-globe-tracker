7343a7aa5daaacee0a4ab992b9b985374f47414b
Tanmay <tanmaylaud@gmail.com>
2020-05-10 23:14:38 +0530

    Updates

---
+++ precache-manifest.0b82d79b2fc3332a89c5c8d54876e3fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5a9d326df21ff5a6ad95143e06a9a4",
+    "url": "/covid19-globe/index.html"
+  },
+  {
+    "revision": "669f60ecfdd420e7085f",
+    "url": "/covid19-globe/static/css/main.2d46099e.chunk.css"
+  },
+  {
+    "revision": "48052edf27369af24cf5",
+    "url": "/covid19-globe/static/js/2.e9e5ce43.chunk.js"
+  },
+  {
+    "revision": "95aed6a00765aeb803e861408bf7612d",
+    "url": "/covid19-globe/static/js/2.e9e5ce43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "669f60ecfdd420e7085f",
+    "url": "/covid19-globe/static/js/main.56efebe5.chunk.js"
+  },
+  {
+    "revision": "afcb0d8c374c493ebc5a",
+    "url": "/covid19-globe/static/js/runtime-main.8dd75717.js"
+  },
+  {
+    "revision": "646e641765bfde4bf557809752329ecc",
+    "url": "/covid19-globe/static/media/world.646e6417.jpg"
+  }
+]);
