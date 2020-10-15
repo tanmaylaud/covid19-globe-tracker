60e86d043fb486ed5d221bb08683277abfc47142
Tanmay <tanmaylaud@gmail.com>
2020-10-15 12:15:03 +0530

    changed source

--- src/Constants.tsx
+++ src/Constants.tsx
@@ -4,6 +4,7 @@ export const BACKGROUND_IMAGE_URL =
   "//cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png";
 export const GEOJSON_URL =
   "https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_110m_admin_0_countries.geojson";
-export const CASES_API = "https://covid3d-backend.now.sh";
+export const CASES_API =
+  "https://raw.githubusercontent.com/wobsoriano/covid3d/master/data.json";
 
 export const FLAG_ENDPOINT = "https://corona.lmao.ninja/assets/img/flags";
